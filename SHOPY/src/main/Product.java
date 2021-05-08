b58f7d0e7da622dec71f3166f54f3d78f14b69ca
ahmedmansour <a.a2051995@gmail.com>
2021-05-09 01:32:30 +0200

    add setter and getter for product class

--- SHOPY/src/main/Product.java
+++ SHOPY/src/main/Product.java
@@ -1,5 +1,30 @@
 package main;
 
 public class Product {
-
+private String name;
+private String desc;
+private float price;
+public Product(String l_name, String l_desc, float l_price) {
+	name = l_name;
+	price = l_price;
+	desc = l_desc;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public float getPrice() {
+	return price;
+}
+public void setPrice(float price) {
+	this.price = price;
+}
+public String getDesc() {
+	return desc;
+}
+public void setDesc(String desc) {
+	this.desc = desc;
+}
 }
