f2be92ece9969e8fec1c2e82f7ad5161ce8483e8
ahmedmansour <a.a2051995@gmail.com>
2021-05-09 01:28:08 +0200

    add Order class

--- SHOPY/src/main/Account.java
+++ SHOPY/src/main/Account.java
@@ -1,5 +1,53 @@
 package main;
-
+import main.Order;
 public class Account {
+	private String name;
+	private String Phone;
+	private String password ;
+	private String email;
+	private String address;
+	public Account(String l_name, String l_email,
+			String l_phone, String l_password, String l_address)
+	{
+		name = l_name;
+		email  = l_email;
+		address = l_address;
+		password = l_password;
+		Phone = l_phone;
+	}
+	public String make(Order order) {
+		
+		return "ok";
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return Phone;
+	}
+	public void setPhone(String phone) {
+		Phone = phone;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
--- SHOPY/src/main/Order.java
+++ SHOPY/src/main/Order.java
@@ -1,5 +1,38 @@
 package main;
 
+import java.time.LocalDate;
+
 public class Order {
+private LocalDate ord_date;
+private LocalDate ord_dilver_date;
+private String status;
+private String ord_code;
+public String addProduct_to_order() {
+	return "ok";
+}
+public LocalDate getOrd_date() {
+	return ord_date;
+}
+public void setOrd_date(LocalDate ord_date) {
+	this.ord_date = ord_date;
+}
+public LocalDate getOrd_dilver_date() {
+	return ord_dilver_date;
+}
+public void setOrd_dilver_date(LocalDate ord_dilver_date) {
+	this.ord_dilver_date = ord_dilver_date;
+}
+public String getStatus() {
+	return status;
+}
+public void setStatus(String status) {
+	this.status = status;
+}
+public String getOrd_code() {
+	return ord_code;
+}
+public void setOrd_code(String ord_code) {
+	this.ord_code = ord_code;
+}
 
 }
