cf84fd641cd49c831514e7d1452c541693ebd63a
ahmedmansour <a.a2051995@gmail.com>
2021-05-09 01:43:12 +0200

    add cart class

---
+++ SHOPY/src/main/Cart.java
@@ -0,0 +1,15 @@
+package main;
+public class Cart {
+ private Product product_list[];
+ public Boolean  add_product(Product l_product) {
+	return true;
+}
+public Boolean remove_product (String Prod_code) {
+	return true;
+}
+ public Boolean Submit() {
+	 
+	 return true;
+ }
+ 
+}
