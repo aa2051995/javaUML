ee7e06dc4586b5128dddfa3de7b67c75e1a05cfe
ahmedmansour <a.a2051995@gmail.com>
2021-05-09 02:43:02 +0200

    implement customer class

---
+++ SHOPY/src/main/Customer.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Customer extends Account{
+	public Customer(String l_name, String l_email,
+				String l_phone, String l_password, String l_address)
+		{
+		super(l_name, l_email, l_phone, l_password, l_address);
+		
+		}
+	
+	
+}
