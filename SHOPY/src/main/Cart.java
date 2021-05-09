334e0cc8c3d80df0148349863c1eba39864392a5
ahmedmansour <a.a2051995@gmail.com>
2021-05-09 02:25:13 +0200

    implement cart class

--- SHOPY/src/main/Cart.java
+++ SHOPY/src/main/Cart.java
@@ -1,15 +1,37 @@
 package main;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class Cart {
- private Product product_list[];
+ private  ArrayList<Product> products;
+ public Cart() {
+	products = new ArrayList<>();
+}
  public Boolean  add_product(Product l_product) {
+	products.add(l_product);
 	return true;
 }
 public Boolean remove_product (String Prod_code) {
-	return true;
+	Product toremove = null ;
+	for(Product prod :products) {
+		if(prod.getProduct_code() == Prod_code)
+			toremove = prod;
+	}
+	if(toremove != null) {
+		products.remove(toremove);
+		return  true;
+	}
+	
+	
+	return false;
 }
- public Boolean Submit() {
-	 
-	 return true;
+ public Boolean Submit(Account account) {
+	 UUID uuid = UUID.randomUUID();
+	 Order order = new Order(uuid.toString(), products);
+	  if(account.make(order) == "ok")
+		  return true;
+	  return false;
  }
  
 }
