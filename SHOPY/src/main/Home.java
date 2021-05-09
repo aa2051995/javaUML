ef479d6e77345c22213bb1b334bc83c87de99ac6
ahmedmansour <a.a2051995@gmail.com>
2021-05-09 02:45:06 +0200

    implement customer buy a product scenario

--- SHOPY/src/main/Home.java
+++ SHOPY/src/main/Home.java
@@ -3,7 +3,11 @@ package main;
 public class Home {
 
 	public static void main(String[] args) {
-	  
+	  Customer myaCustomer =  new Customer("ahmed", "a.a@gmail.com", "01235468", "asdferf56", "8 of may");
+	  Cart mycart = new Cart();
+	  Product myProduct = new Product("lap", "laptop", 10000);
+	  mycart.add_product(myProduct);
+	  mycart.Submit(myaCustomer);
 
 	}
 
