6ec5e52b70c61c12dcd0b3a084e4933110e55dda
ahmedmansour <a.a2051995@gmail.com>
2021-05-09 02:37:01 +0200

    implement merchant class make order method

--- SHOPY/src/main/Merchant.java
+++ SHOPY/src/main/Merchant.java
@@ -1,19 +1,29 @@
 package main;
 
+import java.util.ArrayList;
+
 public class Merchant extends Account {
-	private Product product_list[];
+	private ArrayList<Product> products;
 public Merchant(String l_name, String l_email,
 			String l_phone, String l_password, String l_address)
 	{
 	super(l_name, l_email, l_phone, l_password, l_address);
 	
 	}
-public Boolean resell(String products_codes[]) {
+public Boolean resell(ArrayList<String> products_codes) {
+	
+	for(Product pro: products) {
+		if(products_codes.contains(pro.getProduct_code()))
+			products_codes.remove(pro);
+	}
 	return true;
 	
 }
 @Override
 public String make(Order order) {
+	for(Product pro : order.getAllProduct()) {
+		products.add(pro);
+	}
 	super.make(order);
 	return "ok";
 }
